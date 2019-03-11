8dfb97d80ef8f3a8244e356b1d727d33a679695d
Israel Sekhwela <israelsekhwela@yahoo.com>
2019-03-11 23:12:31 +0200

    API Key

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import Titles from "./components/Titles"
+
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+
+const API_KEY = "5a31f6a280fb3a8f981b81c017bb66ae";
 
 //initialise a componet
 class App extends React.Component {
+  getWeather = async () =>{
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Pretoria,Gauenteng&APPID=${API_KEY}`);
+  }
   render(){
     return (
         <div>
           <Titles />
+          <Form />
+          <Weather />
         </div>
     );
   }
