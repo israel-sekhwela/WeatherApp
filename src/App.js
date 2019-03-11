e69e9b07b132cb37987b72e6691c6842f48447d8
Israel Sekhwela <israelsekhwela@yahoo.com>
2019-03-11 22:20:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//initialise a componet
+class App extends React.Component {
+  render(){
+    return (
+        <div>
+          <h1>Hello!</h1>
+        </div>
+    );
+  }
+}
+
+//make componet available for other files to import
+export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Titles extends React.Component{
+    render(
+        <div>
+            <h1>Weather Finder</h1>
+            <p> Find out temperature, conditions and more...</p>
+        </div>
+    );
+}
